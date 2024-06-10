2e87f4220dc06e9c74e99d1dc3f3d7089dbcd3a9
krakenlake <tews@airpark.de>
2024-06-10 10:45:14 +0200

    added rudimentary 16550 UART driver

--- src/include/vmon/drivers/uart/ns16550.h
+++ src/include/vmon/drivers/uart/ns16550.h
@@ -2,9 +2,9 @@
 #define VMON_UART_H
 
 #define UART_BASE		0x10000000
-#define UART_REG_IER	0x02
+#define UART_REG_IER	2
 #define UART_IRQ_ON		0b00000001
-#define UART_REG_LSR	0x03
+#define UART_REG_LSR	5
 #define UART_MODE_8N1   0b00000011
 
 #define UART_REGSIZE	4
