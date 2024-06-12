b82e174c4056b393c6377343a9065e1ea645a34c
krakenlake <tews@airpark.de>
2024-06-12 10:10:27 +0200

    switching main to use UART driver

--- src/include/vmon/drivers/uart/ns16550.h
+++ src/include/vmon/drivers/uart/ns16550.h
@@ -1,12 +1,13 @@
 #ifndef VMON_UART_H
 #define VMON_UART_H
 
-#define UART_BASE		0x10000000
+//#define UART_REGSIZE	4
+
+#define UART_BASE		0x10000000			# QEMU
 #define UART_REG_IER	2
 #define UART_IRQ_ON		0b00000001
 #define UART_REG_LSR	5
 #define UART_MODE_8N1   0b00000011
 
-#define UART_REGSIZE	4
 
 #endif /* VMON_UART_H */
