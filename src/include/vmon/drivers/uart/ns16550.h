8cf05a6b8872b81ebac4763d280308b8b11c7db6
krakenlake <tews@airpark.de>
2024-06-20 08:49:10 +0200

    added RDR/THR registers by name

--- src/include/vmon/drivers/uart/ns16550.h
+++ src/include/vmon/drivers/uart/ns16550.h
@@ -3,17 +3,25 @@
 
 #ifdef HW_QEMU
 	#define UART_BASE		0x10000000
+	#define UART_REG_RDR	0x00
+	#define UART_REG_THR	0x00
 	#define UART_REG_IER	2
-	#define UART_IRQ_ON		0b00000001
 	#define UART_REG_LSR	5
+
+	#define UART_IRQ_ON		0b00000001
+
 	#define UART_MODE_8N1   0b00000011
 #endif
 
 #ifdef HW_VF2
 	#define UART_BASE		0x10000000
+	#define UART_REG_RDR	0x00
+	#define UART_REG_THR	0x00
 	#define UART_REG_IER	2*4
-	#define UART_IRQ_ON		0b00000001
 	#define UART_REG_LSR	5*4
+
+	#define UART_IRQ_ON		0b00000001
+
 	#define UART_MODE_8N1   0b00000011
 #endif
 
