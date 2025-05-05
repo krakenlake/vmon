3358b20dc62abc57400bc423a5d51c4ff5acefa2
Ben Mesander <benmesander@yahoo.com>
2025-05-05 06:49:16 -0600

    - Finish renaming cmd_B to cmd_SLASH

--- src/include/vmon/config.h
+++ src/include/vmon/config.h
@@ -20,7 +20,6 @@
 
 // which user commands shall be in the binary?
 #define WITH_CMD_A		// assemble	
-#define WITH_CMD_B		// base conversion	
 #define WITH_CMD_C		// copy memory	
 #define WITH_CMD_D		// disassemble	
 #define WITH_CMD_F		// find
@@ -30,6 +29,7 @@
 #define WITH_CMD_M		// memory dump
 #define WITH_CMD_P		// poke
 #define WITH_CMD_X		// exit
+#define WITH_CMD_SLASH		// base conversion	
 
 // if undefined, simply "x0...x31" will be used instead
 #define ABI_REGISTER_NAMES
