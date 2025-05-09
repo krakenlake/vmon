e0523f1a039760f71726b6571a0a48deab849175
krakenlake <tews@airpark.de>
2025-05-09 15:50:36 +0200

    added encoding.h and using it

---
+++ src/include/vmon/encoding.h
@@ -0,0 +1,16 @@
+#ifndef ENCODING_H
+#define ENCODING_H
+
+#define ENC_OFF_MASK			0
+#define ENC_OFF_MATCH			4
+#define ENC_OFF_FUNC_DECODE		8
+#define ENC_OFF_OP_STRING		12
+#define ENC_OFF_FUNC_ASS		16
+#define ENC_OFF_FUNC_ARG1		20
+#define ENC_OFF_FUNC_ARG2		24
+#define ENC_OFF_FUNC_ARG3		28
+#define ENC_OFF_FUNC_ARG4		32
+
+#define ENC_ENTRY_SIZE		36
+
+#endif /* ENCODING_H */
