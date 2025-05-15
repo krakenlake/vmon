1439c92527525ca9fc05e6815b2a52a1445a3994
krakenlake <tews@airpark.de>
2025-05-15 10:08:44 +0200

    encoding table smaller

--- src/include/vmon/encoding.h
+++ src/include/vmon/encoding.h
@@ -5,12 +5,11 @@
 #define ENC_OFF_MATCH			4
 #define ENC_OFF_FUNC_DECODE		8
 #define ENC_OFF_OP_STRING		12
-#define ENC_OFF_FUNC_ASS		16
-#define ENC_OFF_FUNC_ARG1		20
-#define ENC_OFF_FUNC_ARG2		24
-#define ENC_OFF_FUNC_ARG3		28
-#define ENC_OFF_FUNC_ARG4		32
+#define ENC_OFF_FUNC_ARG1		16
+#define ENC_OFF_FUNC_ARG2		20
+#define ENC_OFF_FUNC_ARG3		24
+#define ENC_OFF_FUNC_ARG4		28
 
-#define ENC_ENTRY_SIZE		36
+#define ENC_ENTRY_SIZE		32
 
 #endif /* ENCODING_H */
