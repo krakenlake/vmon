7403e61cbc9896303d3a4abd8466309ddd1c1462
krakenlake <tews@airpark.de>
2025-05-20 21:42:00 +0200

    added register.h

--- src/include/vmon/config.h
+++ src/include/vmon/config.h
@@ -1,30 +1,6 @@
 #ifndef VMON_CONFIG_H
 #define VMON_CONFIG_H
 
-// int register width
-#if XLEN == 32
-	#define	SAVE_X	sw
-	#define LOAD_X	lw
-	#define LWU		lw
-#endif
-#if XLEN == 64
-	#define	SAVE_X	sd
-	#define LOAD_X	ld
-	#define LWU		lwu
-#endif
-#define XLEN_BYTES			(XLEN/8)
-
-// float register width
-#if FLEN == 32
-	#define SAVE_F	fsw
-	#define LOAD_F	flw
-#endif
-#if FLEN == 64
-	#define	SAVE_F	fsd
-	#define LOAD_F	fld
-#endif
-#define FLEN_BYTES			(FLEN/8)
-
 // always use only one of these
 #define M_MODE
 //#define S_MODE
---
+++ src/include/vmon/register.h
@@ -0,0 +1,28 @@
+#ifndef VMON_REGISTER_H
+#define VMON_REGISTER_H
+
+// int register width
+#if XLEN == 32
+	#define	SAVE_X	sw
+	#define LOAD_X	lw
+	#define LWU		lw
+#endif
+#if XLEN == 64
+	#define	SAVE_X	sd
+	#define LOAD_X	ld
+	#define LWU		lwu
+#endif
+#define XLEN_BYTES			(XLEN/8)
+
+// float register width
+#if FLEN == 32
+	#define SAVE_F	fsw
+	#define LOAD_F	flw
+#endif
+#if FLEN == 64
+	#define	SAVE_F	fsd
+	#define LOAD_F	fld
+#endif
+#define FLEN_BYTES			(FLEN/8)
+
+#endif /* VMON_REGISTER_H */
