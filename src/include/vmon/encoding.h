99bd27247835aadab05a77ebbc93a8f0612f7f53
krakenlake <tews@airpark.de>
2025-05-22 22:46:51 +0200

    cleanup

--- src/include/vmon/encoding.h
+++ src/include/vmon/encoding.h
@@ -5,15 +5,8 @@
 #define ENC_OFF_MATCH			4
 #define ENC_OFF_FUNC_DECODE		8
 #define ENC_OFF_OP_STRING		12
-/*
-#define ENC_OFF_FUNC_ARG1		16
-#define ENC_OFF_FUNC_ARG2		20
-#define ENC_OFF_FUNC_ARG3		24
-#define ENC_OFF_FUNC_ARG4		28
+#define ENC_OFF_CTRL			16
 
-#define ENC_ENTRY_SIZE		32
-*/
-#define ENC_OFF_CTRL		16
-#define ENC_ENTRY_SIZE		20
+#define ENC_ENTRY_SIZE			20
 
 #endif /* ENCODING_H */
