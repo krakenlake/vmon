3830a2df4d8d40c4a8a0285ca406f859c0d48dc7
krakenlake <tews@airpark.de>
2025-05-30 18:00:56 +0200

    unified error handling

---
+++ src/include/vmon/error.h
@@ -0,0 +1,12 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+
+#define ERR_CMD_PARAMS			0x01
+#define ERR_CMD_UNKNOWN			0x02
+#define ERR_BREAK_FULL			0x10
+#define ERR_BREAK_ADDR			0x11
+
+#define ERR_UNDEFINED			0xff
+
+#endif /* ERROR_H */
