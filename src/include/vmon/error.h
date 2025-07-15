6c4b98e07bd019acf2c0c003524b721366cb0c4e
krakenlake <tews@airpark.de>
2025-07-15 16:17:13 +0200

    improved error handling

--- src/include/vmon/error.h
+++ src/include/vmon/error.h
@@ -1,11 +1,12 @@
 #ifndef ERROR_H
 #define ERROR_H
 
-
 #define ERR_CMD_PARAMS			0x01
 #define ERR_CMD_UNKNOWN			0x02
 #define ERR_BREAK_FULL			0x03
 #define ERR_INSN_ADDR			0x04
+#define ERR_INSN_NOTFOUND		0x05
+#define ERR_INSN_SYNTAX			0x06
 
 #define ERR_UNDEFINED			0xff
 
